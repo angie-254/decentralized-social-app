7b45f3086a647fe9a153d6a5582e0d618b3135ee
angie-254 <janiceangellah@gmail.com>
2025-12-03 14:23:01 +0300

    Added Posts Content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import {
   urlClient,
   LENS_HUB_CONTRACT_ADDRESS,
@@ -10,13 +10,13 @@ import { ethers } from "ethers";
 import { Box, Button, Image } from "@chakra-ui/react";
 
 function App() {
-  const [account, setAccount ] = useState(null);
+  const [account, setAccount] = useState(null);
   const [profile, setProfile] = useState(null);
   const [posts, setPosts] = useState([]);
 
   async function signIn() {
     const accounts = await window.ethereum.request({
-      method: "eth_requestAccounts"
+      method: "eth_requestAccounts",
     });
 
     setAccount(accounts[0]);
@@ -25,8 +25,8 @@ function App() {
   async function getRecommendedProfiles() {
     // grab recommended profiles in a simple easy way
     const response = await urlClient
-    .query(queryRecommendedProfiles)
-    .toPromise();
+      .query(queryRecommendedProfiles)
+      .toPromise();
 
     //selecting first five profiles
     const profiles = response.data.recommendedProfiles.slice(0, 5);
@@ -35,8 +35,8 @@ function App() {
 
   async function getPosts() {
     const response = await urlClient
-    .query(queryExplorePublications)
-    .toPromise();
+      .query(queryExplorePublications)
+      .toPromise();
 
     const posts = response.data.explorePublications.items.filter((post) => {
       if (post.profile) return post;
@@ -61,30 +61,154 @@ function App() {
     getPosts();
   }, []);
 
+  const parseImageUrl = (profile) => {
+    if (profile) {
+      const url = profile.picture?.original?.url;
+      if (url && url.startsWith("ipfs:")) {
+        const ipfHash = url.split("//")[1];
+        return `https://gateway.pinata.cloud/ipfs/${ipfHash}`;
+      }
+      return url;
+    }
+
+    return "/default-avatar.png";
+  };
+
   return (
     <div className="app">
-      <Box width='100%' backgroundColor="rgba(5, 32, 64, 28)">
-        <Box display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-        width="55%"
-        margin="auto"
-        color="white"
-        padding="10px 0">
+      <Box width="100%" backgroundColor="rgba(5, 32, 64, 28)">
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          width="55%"
+          margin="auto"
+          color="white"
+          padding="10px 0"
+        >
           <Box>
-            <Box fontFamily="DM Serif Display" fontSize="44px" fontStyle="italic">DECENTRA</Box>
+            <Box
+              fontFamily="DM Serif Display"
+              fontSize="44px"
+              fontStyle="italic"
+            >
+              DECENTRA
+            </Box>
             <Box>Decentralized Social Media App</Box>
           </Box>
 
           {account ? (
-            <Box backgroundColor="000" padding="15px" borderRadius="6px">
+            <Box backgroundColor="#000" padding="15px" borderRadius="6px">
               Connected
             </Box>
-          ) : (<Button onClick={signIn} color="rgba(5,32,64)" _hover={{backgroundColor:"#808080"}}>
-            Connect
-          </Button>)}
+          ) : (
+            <Button
+              onClick={signIn}
+              color="rgba(5,32,64)"
+              _hover={{ backgroundColor: "#808080" }}
+            >
+              Connect
+            </Button>
+          )}
+        </Box>
+      </Box>
+      {/* CONTENT */}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        width="55%"
+        margin="35px auto auto auto"
+        color="white"
+      >
+        {/*POSTS */}
+        <Box width="65%" maxWidth="65%" minWidth="65%">
+          {posts.map((post) => (
+            <Box
+              key={post.id}
+              marginBottom="25px"
+              backgroundColor="rgba(5, 32, 64, 28)"
+              padding="40px 30px  40px 25px"
+              borderRadius="6px"
+            >
+              <Box display="flex">
+                {/*PROFILE IMAGE */}
+                <Box width="75px" height="75px" marginTop="8px">
+                  <img
+                    alt="profile"
+                    src={parseImageUrl(post.profile)}
+                    width="75px"
+                    height="75px"
+                    onError={({ currentTarget }) => {
+                      currentTarget.onerror = null; // prevents looping
+                      currentTarget.src = "/default-avatar.png";
+                    }}
+                  />
+                </Box>
+
+                {/*POST CONTENT */}
+                <Box flexGrow={1} marginLeft="20px">
+                  <Box display="flex" justifyContent="space-between">
+                    <Box fontFamily="DM Serif Display" fontSize="24px">
+                      {post.profile?.handle}
+                    </Box>
+                    <Box height="50px" _hover={{ cursor: "pointer" }}>
+                      <Image 
+                      alt="follow-icon"
+                      src="/follow-icon.png" 
+                      width="50px" 
+                      height="50px" 
+                      onClick={() => follow(post.post.id)}
+                      />
+                    </Box>
+                  </Box>
+                  <Box overflowWrap="anywhere" fontSize="14px">
+                    {post.metadata?.content}
+                  </Box>
+
+                </Box>
+              </Box>
+            </Box>
+          ))}
         </Box>
 
+        {/*FRIEND SUGGESTIONS */}
+        <Box width="30%" 
+        backgroundColor="rgba(5, 32, 64, 28)"
+        padding="40px 25px"
+        borderRadius="6px"
+        height="fit-content"
+        >
+          <Box 
+          fontFamily="DM Serif Display"
+          >
+            FRIEND SUGGESTIONS</Box>
+            <Box>
+              {profiles.map((profile, i) => (
+                <Box key={profile.id}
+                 margin="30px 0" 
+                 display="flex" 
+                 alignItems="center" 
+                 height="40px" 
+                _hover={{ color: "#808080", cursor: "pointer" }}>
+
+                  <img
+                    alt="profile"
+                    src={parseImageUrl(profile)}
+                    width="40px"
+                    height="40px"
+                    onError={({ currentTarget }) => {
+                      currentTarget.onerror = null; // prevents looping
+                      currentTarget.src = "/default-avatar.png";
+                    }}
+                  />
+                  <Box marginLeft="25px">
+                    <h4>{profile.name}</h4>
+                    <p>{profile.handle}</p>
+                  </Box>
+                </Box>
+              ))}
+            </Box>
+        </Box>
       </Box>
     </div>
   );
